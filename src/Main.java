3f705ed2738d78fcb62086cac137b50b2d812deb
MartaOstash <marta.if1994@gmail.com>
2023-08-02 20:13:36 -0400

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         }
 
         System.out.println("The code is complete");
+        SolarSystem solarSystem=new Solarsystem;
 
     }
 }
---
+++ src/SolarSystem.java
@@ -0,0 +1,7 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        String color;
+        String tempearture;
+        String nature;
+    }
+}
